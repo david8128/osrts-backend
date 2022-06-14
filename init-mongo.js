5d0d4553965aa245b86d1f360a9c1c9c0ea91b32
David Fonseca <daafonsecato@unal.edu.co>
2022-06-13 19:32:08 -0500

    Changed language to Spanish

---
+++ init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "iicnvte",
+        pwd: "iicnvt2022!",
+        roles: [
+            {
+                role: "readWrite",
+                db: "openSourceRaceTimingSystem"
+            }
+        ]
+    }
+)
